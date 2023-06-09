ef883f98359f3601b04cca310461e60c4d58ddb7
Speykious <speykious@gmail.com>
2023-06-10 01:19:40 +0200

    First draft of event enum

--- src/event.rs
+++ src/event.rs
@@ -1,35 +1,63 @@
-use crate::window::Window;
+use std::path::PathBuf;
 
-#[derive(Debug, PartialEq)]
+use crate::keycode::KeyCode;
+
+#[derive(Clone, Debug)]
+pub enum Event {
+    Resized(u32, u32),
+    Moved(i32, i32),
+
+    CloseRequested,
+    Destroyed,
+
+    FileDropped(PathBuf),
+    FileHovered(PathBuf),
+    FileHoveredCancelled(PathBuf),
+
+    Keyboard(KeyboardEvent),
+    Mouse(MouseEvent),
+    Touch(TouchEvent),
+
+    FocusIn,
+    FocusOut,
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum KeyboardEvent {
+    Key(KeyCode),
+    Char(char),
+    ImeCommit(String),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum MouseButton {
     Left,
-    Middle,
     Right,
+    Middle,
+    Other(u16),
 }
 
-#[derive(Debug, PartialEq)]
-pub enum ClickStatus {
-    /// The mouse/finger just started clicking/pressing
+#[derive(Clone, Copy, Debug)]
+pub enum MouseEvent {
+    ButtonPress(MouseButton),
+    ButtonRelease(MouseButton),
+    CursorMove(f64, f64),
+    CursorIn(f64, f64),
+    CursorOut(f64, f64),
+    Wheel(f64, f64),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum TouchPhase {
     Started,
-    /// The mouse/finger moved on the screen
-    Moved(u32, u32),
-    /// The mouse/finger just stopped clicking/pressing
-    Stopped,
+    Moved,
+    Ended,
+    Cancelled,
 }
 
-pub enum WindowEvent {
-    /// The OS requested a redraw of the window
-    Redraw,
-}
-
-/// Events happening in a window
-pub enum Event {
-    /// The OS requested a redraw of this window
-    Redraw,
-    /// The window should update (but not necessarily redraw its view)
-    Update,
-    /// The user clicked the window
-    Click(ClickStatus, MouseButton),
-    /// The user touched the window
-    Touch(u8, ClickStatus),
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct TouchEvent {
+    phase: TouchPhase,
+    x: f64,
+    y: f64,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 mod app;
-mod event;
+pub mod event;
+pub mod keycode;
 mod native;
 mod window;
 
 pub mod prelude {
     pub use crate::app::App;
-    pub use crate::event::{ClickStatus, Event, MouseButton};
+    pub use crate::event::{Event, MouseButton};
     pub use crate::window::Window;
 }
